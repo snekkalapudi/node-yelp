8df411194701b53ca32346e5b3e7463ad2f9f7ff
Satish Nekkalapudi <bnekkalapudi@paypal.com>
2014-09-18 00:45:43 -0700

    Adding restful support

--- demo/yelp.js
+++ demo/yelp.js
@@ -1,19 +1,44 @@
 var yelp = require("../index").createClient({
-  consumer_key: process.env.CONSUMER_KEY,
-  consumer_secret: process.env.CONSUMER_SECRET,
-  token: process.env.TOKEN,
-  token_secret: process.env.TOKEN_SECRET,
+  consumer_key: 'rlL6wuTbogdyKizHVdzgMQ',
+  consumer_secret: 'kCvu_Fk1Cf1dEmaKnCRbbsaG96I',
+  token: '8gPw6ox904i-nh-Z1XDENK39Y9BMF6Vx',
+  token_secret: 'bTrGqKvqGY8LJ3HXaJ_7QpBwqsE',
   ssl: true
 });
 
 
-yelp.search({term: "food", location: "Montreal"}, function(error, data) {
-  console.log(error);
-  console.log(data);
-});
 
-yelp.business("yelp-san-francisco", function(error, data) {
-  console.log(error);
-  console.log(data);
-});
+
+var url = require('url');
+
+
+
+// yelp.search({term: "food", location: "Montreal"}, function(error, data) {
+//   //console.log(error);
+//   //console.log(data);
+// });
+
+// yelp.business("yelp-san-francisco", function(error, data) {
+//   //console.log(error);
+//   //console.log(data);
+// });
+
+
+var http = require('http');
+http.createServer(function (req, res) {
+
+var url_parts = url.parse(req.url, true);
+var term = url_parts.query.term || 'food',
+	location = url_parts.query.location || 'San Franciso'; 
+
+    res.writeHead(200, {'Content-Type': 'text/json'});
+    console.log(url_parts.query);
+    //console.log(location);
+
+    yelp.search({term: term, location: location}, function(error, data) {
+  		res.end(JSON.stringify(data));
+	});
+
+    
+}).listen(9000, '127.0.0.1');
 
